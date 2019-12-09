1b0b9c201265848c83745344a83909e1435a883d
Jalal Najafi <najafi.jalal.cse@gmail.com>
2019-12-09 22:26:50 +0330

    mongo include

--- src/routers/device.js
+++ src/routers/device.js
@@ -4,12 +4,13 @@ const router = new express.Router()
 
 router.post('/api/device', async (req, res) => {
   const device = new Device(req.query)
-  try {
-    await device.save()
-    res.status(201).send({status: 'ok'})
-  }catch (e){
-    res.status(400).send(e)
-  }
+res.send(device)
+  // try {
+  //   await device.save()
+  //   res.status(201).send({status: 'ok'})
+  // }catch (e){
+  //   res.status(400).send(e)
+  // }
 })
 
 module.exports = router
