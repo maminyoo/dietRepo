64f3841f5474f7b574d5267d8d74ae7da8b5d0f1
Jalal Najafi <najafi.jalal.cse@gmail.com>
2019-12-10 08:27:48 +0330

    slahiye

--- src/routers/device.js
+++ src/routers/device.js
@@ -4,13 +4,12 @@ const router = new express.Router()
 
 router.post('/api/device', async (req, res) => {
   const device = new Device(req.query)
-res.send(device)
-  // try {
-  //   await device.save()
-  //   res.status(201).send({status: 'ok'})
-  // }catch (e){
-  //   res.status(400).send(e)
-  // }
+  try {
+    await device.save()
+    res.status(201).send({status: 'ok'})
+  }catch (e){
+    res.status(400).send(e)
+  }
 })
 
 module.exports = router
