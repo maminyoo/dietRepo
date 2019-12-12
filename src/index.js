c77ad4567f3631a77d88656e8d8b2f1cff6872f4
Jalal Najafi <najafi.jalal.cse@gmail.com>
2019-12-12 14:24:19 +0330

    100

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ require('./globals/global')
 
 const app = require('./app')
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3337
 app.listen(port, () => print('Server is up on port ' + port))
