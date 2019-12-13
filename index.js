f9c13ef8ea597e685bbfd715b0dc546774aaef03
Jalal Najafi <najafi.jalal.cse@gmail.com>
2019-12-13 20:58:44 +0330

    a

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ require('./src/global')
 require('dotenv').config()
 const app = require('./src/app')
 const port = process.env.PORT
-app.listen(port, () => print('Server port : ' + port))
+app.listen(port, () => print('Server OnPort : ' + port))
