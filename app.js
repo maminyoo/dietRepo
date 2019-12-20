3d8461490f83f878ccbd64a15ad4d6ad3952e420
Ali Ghanavatian <ghanavatian.ali@gmail.com>
2019-12-20 23:24:40 +0330

    need to specify what to listen to

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const login    = require('./routers/login')
 const payment  = require('./routers/payment')
 
 const app = express()
+app.listen(process.env.PORT || 80, '0.0.0.0');
 
 app.use(express.json())
 app.use(device)
