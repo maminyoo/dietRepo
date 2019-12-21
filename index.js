d408b78fc76cc30ff85e0cb9dd85194a8d326acd
Apple <apple@Apples-iMac.local>
2019-12-21 18:03:06 +0330

    new

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('./src/global')
 require('dotenv').config()
 const app = require('./src/app')
-const port = process.env.PORT
-app.listen(port, () => print('Server OnPort : ' + port))
+// const port = process.env.PORT
+// app.listen(port, () => print('Server OnPort : ' + port))
--- src/app.js
+++ src/app.js
@@ -6,11 +6,13 @@ const login    = require('./routers/login')
 const payment  = require('./routers/payment')
 
 const app = express()
+app.listen(process.env.PORT || 80, '0.0.0.0');
 
 app.use(express.json())
 app.use(device)
 app.use(register)
 app.use(login)
 app.use(payment)
+
 
 module.exports = app
