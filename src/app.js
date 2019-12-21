3ba7c4277318567f2ef8bde0b2aaa5f0e7ae9d42
Apple <apple@Apples-iMac.local>
2019-12-21 18:24:52 +0330

    listen

--- src/app.js
+++ src/app.js
@@ -4,14 +4,11 @@ const device   = require('./routers/device')
 const register = require('./routers/register')
 const login    = require('./routers/login')
 const payment  = require('./routers/payment')
-
 const app = express()
-app.listen(process.env.PORT || 80, '0.0.0.0');
-
 app.use(express.json())
 app.use(device)
 app.use(register)
 app.use(login)
 app.use(payment)
-
+app.listen(process.env.PORT, '0.0.0.0');
 module.exports = app
